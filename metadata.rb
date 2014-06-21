187fde45763179cf623df66c64e92fcebbd7b506
Bryan Baugher <Bryan.Baugher@Cerner.com>
2014-06-21 09:24:03 -0500

    Released 0.1.1 and bumped version to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,4 @@ maintainer_email 'Bryan.Baugher@Cerner.com'
 license          'All rights reserved'
 description      'Installs/Configures Apache Zookeeper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
-
-depends "java"
-
-%w{ ubuntu debian redhat centos }.each do |os|
-  supports os
-end
+version          '0.2.0'
