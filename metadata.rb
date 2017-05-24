977d262a34995cdc6a17ee3f27ecf84e47333a0f
Bryan Baugher <bryan.baugher@cerner.com>
2017-05-24 09:31:03 -0500

    Fixed foodcritic lint

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
 name             'apache_zookeeper'
 maintainer       'Bryan Baugher'
 maintainer_email 'Bryan.Baugher@Cerner.com'
-license          'All rights reserved'
+license          'MIT'
 description      'Installs/Configures Apache Zookeeper'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url       'https://github.com/bbaugher/apache_zookeeper/issues'
 source_url       'https://github.com/bbaugher/apache_zookeeper'
-
+chef_version     '>= 12.5'
 
 depends "java"
 depends "ulimit"
