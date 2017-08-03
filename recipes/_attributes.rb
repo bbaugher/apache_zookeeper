6a8f82b684fd274f819bf6c989eb755fe274a2b0
gfrntz <chainwolf@gmail.com>
2017-08-03 14:03:55 +0300

    fix nil var

--- recipes/_attributes.rb
+++ recipes/_attributes.rb
@@ -12,7 +12,6 @@ when 'source'
   base_dir = ::File.join(node['apache_zookeeper']['install_dir'], 'current')
   local_state_dir = node['apache_zookeeper']['local_state_dir']
 
-  node.default['apache_zookeeper']['config_dir'] = ::File.join(base_dir, 'conf')
   node.default['apache_zookeeper']['bin_dir'] = ::File.join(base_dir, 'bin')
   node.default['apache_zookeeper']['data_dir'] = ::File.join(local_state_dir, 'data')
   node.default['apache_zookeeper']['log_dir'] = ::File.join(local_state_dir, 'log')
@@ -25,6 +24,8 @@ when 'package'
   node.default['apache_zookeeper']['data_dir'] = local_state_dir
   node.default['apache_zookeeper']['log_dir'] = '/var/log/zookeeper'
 end
+
+node.default['apache_zookeeper']['config_dir'] = ::File.join(base_dir, 'conf')
 
 node.default['apache_zookeeper']['env_vars']['ZOO_LOG_DIR'] = node['apache_zookeeper']['log_dir']
 
