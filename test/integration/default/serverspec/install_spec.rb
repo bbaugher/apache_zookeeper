a8f0b8fdf37fe36fb91072f4a54cf45155df376f
Bryan Baugher <bryan.baugher@cerner.com>
2017-08-04 15:13:51 -0500

    Fixed kitchen test

--- test/integration/default/serverspec/install_spec.rb
+++ test/integration/default/serverspec/install_spec.rb
@@ -11,10 +11,10 @@ describe file('/opt/zookeeper/current') do
   it { should be_symlink }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
-  it { should be_linked_to '/opt/zookeeper/zookeeper-3.4.6' }
+  it { should be_linked_to '/opt/zookeeper/zookeeper-3.4.10' }
 end
 
-describe file('/opt/zookeeper/zookeeper-3.4.6') do
+describe file('/opt/zookeeper/zookeeper-3.4.10') do
   it { should exist }
   it { should be_directory }
   it { should be_owned_by 'zookeeper' }
@@ -26,7 +26,7 @@ describe 'zookeeper' do
     # Ensure we reload ruby's usernames/groups
     Etc.endgrent
     Etc.endpwent
-    Dir["/opt/zookeeper/current/**/*"].reject{ 
+    Dir["/opt/zookeeper/current/**/*"].reject{
         |f| f['/opt/zookeeper/current/conf']}.each do |filePath|
       expect(Etc.getpwuid(File.stat(filePath).uid).name).to eq("zookeeper")
       expect(Etc.getgrgid(File.stat(filePath).gid).name).to eq("zookeeper")
@@ -40,4 +40,3 @@ describe 'zookeeper' do
   end
 
 end
-
